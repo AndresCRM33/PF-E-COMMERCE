7ceef32c44c1ff58810d442e742d044db36c4489
Octasanchezz <101231228+Octasanchezz@users.noreply.github.com>
2022-12-08 01:10:19 -0300

    Creacion Modelo Post y Carpeta Components y Redux

---
+++ my-app/api/src/models/post.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose")
+
+const PostScheme = new mongoose.Schema(
+    {
+        rol: {
+            type: ["dueño", "inmobiliaria"],
+            required: true
+        },
+        tipoBusqueda: {
+            type: ["comprar", "alquilar", "temporal"],
+            required: true
+        },
+        tipoVivienda: {
+            type: ["departamento", "casa", "ph", "terreno", "local comercial", "oficina"],
+            required: true
+        },
+        tipoAmbiente: {
+            type: ["2 ambiente", "3 ambiente", "4 ambiente"],
+            required: true
+        },
+        ciudad: {
+            type: ["Buenos Aires", "Catamarca", "Chaco", "Chubut", "Cordoba", "Corrientes", "Entre Rios", "Formosa", "Jujuy", "La Pampa",
+                "La Rioja", "Mendoza", "Misiones", "Neuquen", "Rio Negro", "Salta", "San Juan", "San Luis", "Santa Cruz",
+                "Santa Fe", "Santiago del Estero", "Tierra del Fuego", "Tucuman"],
+            required: true
+        },
+        caracteristicas: {
+            type: ["parking", "asador", "pileta", "zom", "hogar"]
+        },
+        imagen: {
+            type: String
+        },
+        valor: {
+            type: String
+        },
+        contacto: {
+            type: ["celular", "mail"]
+        },
+        celular: {
+            type: String
+        },
+        mail: {
+            type: String
+        }
+    },
+    {
+        timestamps: true,
+    }
+)
